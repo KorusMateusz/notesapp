76eb4099f7abaf36d5b7bf68a2b26a608b46b9c9
Varret <varret@wp.pl>
2019-02-27 16:38:20 +0100

    Implemented strategySetup function to automagically setup different passport social OAuth strategies which resulted in file being almost half its previous size. Feeling so clever :)

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -14,70 +14,40 @@ passport.deserializeUser((id, done)=>{
   })
 });
 
-
-passport.use(new GitHubStrategy({
-  callbackURL: "/auth/github/redirect",
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET
-}, (accessToken, refreshToken, profile, done) => {
-  User.findOne({strategy: "google", authId: profile.id}).then((currentUser)=>{
-    if(currentUser){
-      return done(null, currentUser);
-    }
-    new User({
-      strategy: "google",
-      username: profile.displayName,
-      authId: profile.id,
-      registered: new Date()
-    }).save().then((newUser)=>{
-      console.log("new user created: " + newUser);
-      return done(null, newUser)
-    });
-  });
-  })
-);
-
-passport.use(new GoogleStrategy({
-    callbackURL: "/auth/google/redirect",
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET
-  }, (accessToken, refreshToken, profile, done) => {
-  User.findOne({strategy: "google", authId: profile.id}).then((currentUser)=>{
-    if(currentUser){
-      return done(null, currentUser);
-    }
-    new User({
-      strategy: "google",
-      username: profile.displayName,
-      authId: profile.id,
-      registered: new Date()
-    }).save().then((newUser)=>{
-      console.log("new user created: " + newUser);
-      return done(null, newUser)
-    });
-  });
-  })
-);
-
-passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID,
-  clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "/auth/facebook/redirect"
-  }, (accessToken, refreshToken, profile, done) => {
-  console.log(profile);
-    User.findOne({strategy: "facebook", authId: profile.id}).then((currentUser)=>{
-      if(currentUser){
-        return done(null, currentUser);
-      }
-      new User({
-        strategy: "facebook",
-        username: profile.displayName,
-        authId: profile.id,
-        registered: new Date()
-      }).save().then((newUser)=>{
-        console.log("new user created: " + newUser);
-        return done(null, newUser)
+function strategySetup(strategy, clientID, clientSecret){
+  // returns a list of parameters needed for strategy setup that is later deconstructed
+  const callbackURL = `/auth/${strategy}/redirect`;
+  return[{
+    callbackURL: callbackURL,
+    clientID: clientID,
+    clientSecret: clientSecret
+  },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({strategy: strategy, authId: profile.id}).then((currentUser)=>{
+        if(currentUser){
+          return done(null, currentUser);
+        }
+        new User({
+          strategy: strategy,
+          username: profile.displayName,
+          authId: profile.id,
+          registered: new Date()
+        }).save().then((newUser)=>{
+          return done(null, newUser)
+        });
       });
-    });
-  })
-);
+    }
+  ]
+}
+
+passport.use(new GitHubStrategy(
+  ...strategySetup("github", process.env.GITHUB_CLIENT_ID, process.env.GITHUB_CLIENT_SECRET)
+));
+
+passport.use(new GoogleStrategy(
+  ...strategySetup("google", process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET)
+));
+
+passport.use(new FacebookStrategy(
+  ...strategySetup("facebook", process.env.FACEBOOK_APP_ID, process.env.FACEBOOK_APP_SECRET)
+));
